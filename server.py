c10af784ad313c64ccc41990fce24c11f914d574
GAUTAM JHALARIA <jhalariagautam@gmail.com>
2025-06-07 16:21:20 +0530

    changed transport from streamble http to sse

--- server.py
+++ server.py
@@ -76,4 +76,5 @@ def decline_pr(decline: DeclinePullRequest) -> dict:
     return decline_pull_request(decline)
 
 if __name__ == "__main__":
-     mcp.run(transport="streamable-http")
+     mcp.settings.port = 3005
+     mcp.run(transport="sse")
